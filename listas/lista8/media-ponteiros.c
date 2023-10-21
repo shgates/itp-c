d3cdd6a08da44266ea76664ffe414421bdddadb6
shgates <sousa.gates@gmail.com>
2023-10-21 15:46:45 -0300

    done

--- listas/lista8/media-ponteiros.c
+++ listas/lista8/media-ponteiros.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+void calcular_media(int *A, int *B)
+{
+    int media = (*A+*B)/2;
+    int resto = (*A+*B)%2;
+    
+    if(*A>*B)
+    {
+        *A = resto;
+        *B = media;
+        
+    }
+    else if(*B>*A)
+    {
+        *A = media;
+        *B = resto;
+    }
+}
 
 int main() {
-
-
+    
+    int A, B;
+    scanf(" %d %d", &A, &B);
+    
+    calcular_media(&A, &B);
+    
+    printf("A = %d\nB = %d", A, B);
+    
     return 0;
 }
